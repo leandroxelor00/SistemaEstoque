e381c77b444a0fd79851c190cdc2298d6d9ce93f
leandroxelor00 <leandroxelor00@gmail.com>
2026-05-13 08:18:05 -0300

    comentando possiveis problemas
    comentando algumas partes de codigos que estao dando problema na execução do programa em outras maquinas

--- src/model/DAO/stockDAO.py
+++ src/model/DAO/stockDAO.py
@@ -24,22 +24,22 @@ class EstoqueDAO(BaseDB):
         novaLista[index] = produto
         self.saveList(novaLista)
 
-    def procurarProdNoEstoque(self, id):
-        produto = [produto for produto in self.verEstoque() if produto["idProd"] == id]
-        if produto:
-            for produto in produto:
-                return f"ID: {produto["idProd"]} | Quantidade: {produto["quantidade"]}"
-        else:
-            raise ValueError("Produto não encontrado por esse ID")
-
-
-
-if __name__ == '__main__':
-    es1 = EstoqueDAO()
-    prod = {
-        "idProd": 5,
-        "quantidade": 0
-    }
-    es1.addNovoProdEstoque(prod)
-    print(es1.verEstoque())
+#     def procurarProdNoEstoque(self, id):
+#         produto = [produto for produto in self.verEstoque() if produto["idProd"] == id]
+#         if produto:
+#             for produto in produto:
+#                 return f"ID: {produto["idProd"]} | Quantidade: {produto["quantidade"]}"
+#         else:
+#             raise ValueError("Produto não encontrado por esse ID")
+#
+#
+#
+# if __name__ == '__main__':
+#     es1 = EstoqueDAO()
+#     prod = {
+#         "idProd": 5,
+#         "quantidade": 0
+#     }
+#     es1.addNovoProdEstoque(prod)
+#     print(es1.verEstoque())
 
